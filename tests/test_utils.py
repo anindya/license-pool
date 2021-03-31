754eb2045413a47a094d07c57d249bb18ba40c98
ruoyi <endvroy@gmail.com>
2021-03-30 20:31:22 -0400

    updated tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,3 +8,20 @@ res = requests.post('http://localhost:5000/license/request',
                     json={'username': 'Emma',
                           'password': 'abcd',
                           'container_id': 'foo'})
+
+
+def request(uname, password, cid):
+    res = requests.post('http://localhost:5000/license/request',
+                        json={'username': uname,
+                              'password': password,
+                              'container_id': cid})
+    return res
+
+
+res = request('Emma', 'abcd', 'foo')
+res = request('Emma', 'abcd', 'bar')
+
+res = request('John', 'efgh', 'baz')
+res = request('John', 'efgh', 'foobar')
+res = request('John', 'efgh', 'boom')
+res = request('John', 'efgh', 'oops')
