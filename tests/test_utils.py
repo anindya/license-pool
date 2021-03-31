0b9b94f4d3f75d618b9b6645521db4678058a309
ruoyi <endvroy@gmail.com>
2021-03-30 20:56:41 -0400

    tests are now semi-automatic

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,11 +4,6 @@ import json
 res = requests.get('http://localhost:5000/licenses')
 licenses = json.loads(res.text)
 
-res = requests.post('http://localhost:5000/license/request',
-                    json={'username': 'Emma',
-                          'password': 'abcd',
-                          'container_id': 'foo'})
-
 
 def request(uname, password, cid):
     res = requests.post('http://localhost:5000/license/request',
@@ -19,9 +14,14 @@ def request(uname, password, cid):
 
 
 res = request('Emma', 'abcd', 'foo')
+assert res.status_code == 200
 res = request('Emma', 'abcd', 'bar')
-
+assert res.status_code == 403
 res = request('John', 'efgh', 'baz')
+assert res.status_code == 200
 res = request('John', 'efgh', 'foobar')
+assert res.status_code == 200
 res = request('John', 'efgh', 'boom')
+assert res.status_code == 200
 res = request('John', 'efgh', 'oops')
+assert res.status_code == 403
