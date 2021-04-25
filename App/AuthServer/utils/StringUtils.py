e0040523c6cb6ef222bc41ac7ee02ab88a7aa0f5
Ling-I Huang <liondonaugust810810@gmail.com>
2021-04-24 20:43:00 -0400

    install the licensing package using pip install and remove the package in the directory

--- App/AuthServer/utils/StringUtils.py
+++
@@ -1,6 +0,0 @@
-import random
-import string
-
-letters_and_digits = string.ascii_uppercase + string.ascii_lowercase + string.digits
-def getRandomString(n):
-    return ''.join(random.SystemRandom().choice(letters_and_digits) for _ in range(n))
