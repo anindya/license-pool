980f140052854caabb9d39026082d4a29c7b5266
Ling-I Huang <liondonaugust810810@gmail.com>
2021-05-05 17:36:21 -0400

    obfuscate App/

--- App/obfuscate.sh
+++ App/obfuscate.sh
@@ -1,5 +1,7 @@
-pyarmor obfuscate --recursive App/app.py
-cp App/Dockerfile dist/
-cp App/requirements.txt dist/
+pyarmor obfuscate --recursive --output App_dist/ App/main.py
+cp App/Dockerfile App_dist/
+cp App/requirements.txt App_dist/
+cp -r App/app_pkg App_dist/
+cp -r App/license-pkg App_dist/
 #rm -rf dist/pytransform
 #cp -r pytransform dist/pytransform
